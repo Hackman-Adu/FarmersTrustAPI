04bb1e49fb8f8c5edfa4e6100b9426d17a04ef4e
Hackman Adu Gyamfi <hgyamfi@IM-21-B1-032.local>
2021-05-05 16:53:34 +0000

    Controller

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -4,37 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\passwordReset;
 use Illuminate\Http\Request;
+use App\Http\Controllers\SMSController;
 
 class PasswordResetController extends Controller
 {
-    public function sendSMS($number, $message)
-    {
-        $private_key = '2021050498000902';
-        $public_key = '085b9a34f27d6ec1';
-        $sender = 'FarmerTrust';
-        $message = rawurlencode($message);
-        $url = 'http://api.msmpusher.net/v1/send';
-        $data = [
-            "privatekey" => $private_key,
-            "publickey" => $public_key,
-            "sender" => $sender,
-            "numbers" => $number,
-            "message" => $message
-        ];
-        $curl = curl_init($url);
-
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, [
-            "Content-Type: application/json",
-            "Accept: application/json"
-        ]);
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return $response;
-    }
-
     public function sendCode(Request $request)
     {
         $reset = new passwordReset();
@@ -45,15 +18,14 @@ class PasswordResetController extends Controller
         $reset->date = date("Y-m-d H:i:s");
         if (!$reset::where("user_id", $reset->user_id)->first()) {
             if ($reset->save()) {
-                return response()->json(["message" => "successful", "sms" => json_decode($this->sendSMS($number, $message))]);
+                return response()->json(["message" => "successful", "sms" => json_decode(SMSController::sendSMS($number, $message))]);
             } else {
-                return response()->json(["message" => "failed", "sms" => json_decode($this->sendSMS($number, $message))]);
+                return response()->json(["message" => "failed", "sms" => json_decode(SMSController::sendSMS($number, $message))]);
             }
         } else {
             return ($this->resendCode($reset->user_id, $reset->code, $number, $message));
         }
     }
-
 
     //verifying user's password reset code
     public function verifyCode($id, $resetCode)
@@ -76,9 +48,9 @@ class PasswordResetController extends Controller
         $code->date = $date;
         $code->code = $newCode;
         if ($code->save()) {
-            return response()->json(['message' => "successful", "sms" => json_decode($this->sendSMS($number, $message))]);
+            return response()->json(['message' => "successful", "sms" => json_decode(SMSController::sendSMS($number, $message))]);
         } else {
-            return response()->json(['message' => "failed", "sms" => json_decode($this->sendSMS($number, $message))]);
+            return response()->json(['message' => "failed", "sms" => json_decode(SMSController::sendSMS($number, $message))]);
         }
     }
 }
---
+++ app/Http/Controllers/SMSController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SMSController extends Controller
+{
+    public static function sendSMS($number, $message)
+    {
+        $private_key = '2021050498000902';
+        $public_key = '085b9a34f27d6ec1';
+        $sender = 'FarmerTrust';
+        $message = rawurlencode($message);
+        $url = 'http://api.msmpusher.net/v1/send';
+        $data = [
+            "privatekey" => $private_key,
+            "publickey" => $public_key,
+            "sender" => $sender,
+            "numbers" => $number,
+            "message" => $message
+        ];
+        $curl = curl_init($url);
+
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            "Content-Type: application/json",
+            "Accept: application/json"
+        ]);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
+    }
+}
